7cf34c2abfbfabfebd0cf36bb2227566aeeb8f69
rpmullig <robertpmulligan@gmail.com>
2023-11-11 23:05:07 -0500

    Stage 3. Working without the guide's support.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import socket
 
+OK_HTTP_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n"
+NOT_FOUND_HTTP_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n"
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +12,20 @@ def main():
     client_socket, address = server_socket.accept() # wait for client
 
     request = client_socket.recv(1024)
-
-    http_response = "HTTP/1.1 200 OK\r\n\r\n"
+    http_response = parse_request_path(request.decode())
 
     client_socket.sendall(http_response.encode())
     client_socket.close()
 
+def parse_request_path(decoded_request_str: str) -> str:
+    lines = decoded_request_str.split('\r\n')
+    line_one_array = lines[0].split(' ')
+    path = line_one_arr[2]
 
+    if path == '/':
+        return OK_HTTP_RESPONSE
+    else:
+        return NOT_FOUND_HTTP_RESPONSE
 
 if __name__ == "__main__":
     main()
