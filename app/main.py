d166a3756745e5cdabd1415a4a4fcfde641c0c7a
rpmullig <robertpmulligan@gmail.com>
2023-11-11 22:48:30 -0500

    Stage 2 commit with most of the instructions provided.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-# Uncomment this to pass the first stage
 import socket
 
 
@@ -6,10 +5,16 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, address = server_socket.accept() # wait for client
+
+    request = client_socket.recv(1024)
+
+    http_response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
+
 
 
 if __name__ == "__main__":
